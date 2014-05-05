c903b32336ce5c794d6e8a5bce0292befbec8e58
Jack Lin <f24u23@berkeley.edu>
2014-05-05 15:52:53 -0700

    fix makefile .git extention in the github repository address to make devserver

--- website/publishconf.py
+++ website/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://benoitdherin.github.io/data-analysis-template/'
+SITEURL = 'https://github.com/jack-lin-r/data-analysis-template'
 
 RELATIVE_URLS = False
 
